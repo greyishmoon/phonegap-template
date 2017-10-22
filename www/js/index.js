f58c5e519787125ed016460e917c3a7b80a0c9b0
greyishmoon <chris@blythe.plus.com>
2017-10-22 19:29:20 +0100

    JQuery onReady added

--- www/js/index.js
+++ www/js/index.js
@@ -1,21 +1,37 @@
 
+//----------- CORDOVA ONLOAD ---------------- //
+
 // Cordova device event listener - will not work in browser
 document.addEventListener("deviceready", onDeviceReady, false);
 		
-	
-
-
-
 // Cordova device event triggered function
 function onDeviceReady() {
 	
 	// Add other event listeners here if needed (pause, resume, backbutton etc)
 	
     // updates display    
-	updateDisplay();
+	// updateDisplay();
 	    
 	console.log("device ready");
 }
+
+//----------- CORDOVA ONLOAD ---------------- //
+
+//----------- HTML ONLOAD ---------------- //
+
+// JQuery ready event listener
+$( document ).ready( onReady );
+// or... $( window ).on( "load", readyFn );
+
+// JQuery DOM loaded event triggered function
+function onReady( jQuery  ) {
+    // MAIN CODE HERE
+    console.log("onReady");
+    
+    updateDisplay();
+}
+
+//----------- HTML ONLOAD ---------------- //
 
 
 // USE FOR ALL DISPLAY CODE
