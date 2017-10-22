abba7e3bf7f54a2bf5b4c4951810a30281d84621
greyishmoon <chris@blythe.plus.com>
2017-10-22 19:14:38 +0100

    Cleaned javascript - cordova only

--- www/js/index.js
+++ www/js/index.js
@@ -1,44 +1,24 @@
-var paused_count =0;
-var resumed_count = 0;
-var launched_count = 0;
 
+// Cordova device event listener - will not work in browser
 document.addEventListener("deviceready", onDeviceReady, false);
 		
 	
-function updateDisplay() {
-	$("#launched").text("Application launched: " + launched_count);
-	$("#resumed").text("Application paused: " + paused_count);
-	$("#paused").text("Application resumed: " + resumed_count);
+
+
+
+// Cordova device event triggered function
+function onDeviceReady() {
+	
+	// Add other event listeners here if needed (pause, resume, backbutton etc)
+	
+    // updates display    
+	updateDisplay();
+	    
+	console.log("device ready");
 }
 
 
-// device APIs are available
-//
-    function onDeviceReady() {
-	
-	document.addEventListener("resume", onResume, false);
-	document.addEventListener("pause", onPause, false);
-	
-	launched_count++;
-	updateDisplay();
-	    
-	alert("device ready");
-    }
-
-
-    function onPause() {
-	
-	paused_count++;
-	updateDisplay();
-	    
-	alert("pause");
-    }
-	
-
-    function onResume() {
-		
-	resumed_count++;
-	updateDisplay();
-	    
-	alert("resume");
-    }
+// USE FOR ALL DISPLAY CODE
+function updateDisplay() {
+	$("#linked").text("Text replaced by updateDisplay");
+}
