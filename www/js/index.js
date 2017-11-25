abc8ce8ff1a3e4e76576a81521a8a711f10fe74b
greyishmoon <chris@blythe.plus.com>
2017-11-25 16:24:17 +0000

    js tidy

--- www/js/index.js
+++ www/js/index.js
@@ -1,38 +1,25 @@
 //----------- CORDOVA ONLOAD ---------------- //
 // Cordova device event listener - will not work in browser
-document.addEventListener("deviceready", onDeviceReady, false);
-
-function onDeviceReady() {
-    console.log("Cordova onDeviceReady");
-    initialize();
-}
+document.addEventListener("deviceready", initialize, false);
 
 //----------- HTML ONLOAD ---------------- //
 // JQuery ready event listener
-$(document).ready(onReady);         // or... $(window).on( "load", readyFn );
-
-function onReady(jQuery) {
-    console.log("jQuery onReady");
-    initialize();
-}
+$(document).ready(initialize); // or... $(window).on( "load", readyFn );
 
 //----------- JQM ONLOAD ---------------- //
 // JQuery Mobile pageloaded event listener
-$(document).on('pageshow', '#pageone', onLoad);
+$(document).on('pageshow', '#pageone', initialize);
 
-function onLoad() {
-    console.log("jQuery Mobile onLoad");
-    initialize();
-}
 
 function initialize() {
-    console.log("initialize");
     // EVENT LISTENERS HERE
 
     // DEMO
     //set up click event handling for button
     $('#linkedButton').on("click", updateText);
     $("#linkedText").text("Text replaced by initialize");
+
+    console.log("initialised");
 }
 
 function updateText() {
