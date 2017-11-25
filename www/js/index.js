7913688a91680c50b68aa3f1952b68a30fff37ab
greyishmoon <chris@blythe.plus.com>
2017-11-25 12:08:57 +0000

    tidy

--- www/js/index.js
+++ www/js/index.js
@@ -1,18 +1,17 @@
-
 //----------- CORDOVA ONLOAD ---------------- //
 
 // Cordova device event listener - will not work in browser
 document.addEventListener("deviceready", onDeviceReady, false);
-		
+
 // Cordova device event triggered function
 function onDeviceReady() {
-	
-	// Add other event listeners here if needed (pause, resume, backbutton etc)
-	
+
+    // Add other event listeners here if needed (pause, resume, backbutton etc)
+
     // updates display    
-	// updateDisplay();
-	    
-	console.log("device ready");
+    // updateDisplay();
+
+    console.log("device ready");
 }
 
 //----------- CORDOVA ONLOAD ---------------- //
@@ -20,14 +19,14 @@ function onDeviceReady() {
 //----------- HTML ONLOAD ---------------- //
 
 // JQuery ready event listener
-$( document ).ready( onReady );
+$(document).ready(onReady);
 // or... $( window ).on( "load", readyFn );
 
 // JQuery DOM loaded event triggered function
-function onReady( jQuery  ) {
+function onReady(jQuery) {
     // MAIN CODE HERE
     console.log("onReady");
-    
+
     updateDisplay();
 }
 
@@ -36,5 +35,5 @@ function onReady( jQuery  ) {
 
 // USE FOR ALL DISPLAY CODE
 function updateDisplay() {
-	$("#linked").text("Text replaced by updateDisplay");
+    $("#linked").text("Text replaced by updateDisplay");
 }
